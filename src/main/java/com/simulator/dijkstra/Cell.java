af6cd08ffe57818ee41ca4b526e5a9adb783424d
davide954 <davidepolato05@gmail.com>
2025-08-12 17:48:46 +0200

    feat: implement equals() and hashCode() for Cell position identity

--- src/main/java/com/simulator/dijkstra/Cell.java
+++ src/main/java/com/simulator/dijkstra/Cell.java
@@ -1,5 +1,7 @@
 package com.simulator.dijkstra;
 
+import java.util.Objects;
+
 public class Cell implements Comparable<Cell> {
     public int row;
     public int col;
@@ -46,9 +48,36 @@ public class Cell implements Comparable<Cell> {
      * @return A negative integer, zero, or a positive integer as this object
      *         is less than, equal to, or greater than the specified object.
      */
-
     @Override
     public int compareTo(Cell other) {
         return Integer.compare(this.distance, other.distance);
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * Required for correct functioning with Set and Map collections.
+     * 
+     * @param o The reference object with which to compare.
+     * @return true if this object is the same as the obj argument; false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Cell cell = (Cell) o;
+        return row == cell.row && col == cell.col;
+    }
+
+    /**
+     * Returns a hash code value for the object.
+     * Required for correct functioning with Set and Map collections.
+     * 
+     * @return A hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
