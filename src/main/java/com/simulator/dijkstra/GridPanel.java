20504526e33a7e9d050000ed9adbb14d433916c8
davide954 <davidepolato05@gmail.com>
2025-08-12 18:46:18 +0200

    Implement rendering of grid cells including walls, paths, start/end and weights

--- src/main/java/com/simulator/dijkstra/GridPanel.java
+++ src/main/java/com/simulator/dijkstra/GridPanel.java
@@ -43,4 +43,77 @@ public class GridPanel extends JPanel {
         setPreferredSize(new Dimension(grid[0].length * CELL_SIZE, grid.length * CELL_SIZE));
         addMouseListeners();
     }
+
+    // Mouse listeners will be added in a subsequent commit
+
+    /**
+     * Paints the component. This method is called by the Swing framework.
+     * 
+     * @param g The Graphics object to protect.
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Iterate through all cells and draw them
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                Cell cell = grid[row][col];
+                drawCell(g2d, cell);
+            }
+        }
+    }
+
+    /**
+     * Draws a single cell on the grid.
+     * 
+     * @param g2d  The Graphics2D object to draw on.
+     * @param cell The Cell object to draw.
+     */
+    private void drawCell(Graphics2D g2d, Cell cell) {
+        int x = cell.col * CELL_SIZE;
+        int y = cell.row * CELL_SIZE;
+
+        Color cellColor = COLOR_EMPTY; // Default color
+
+        // Determine cell color based on its state, prioritizing path, current, visited,
+        // then empty
+        if (cell.isWall) {
+            cellColor = COLOR_WALL;
+        } else if (cell.isPath) {
+            cellColor = COLOR_PATH;
+        } else if (cell.isCurrent) {
+            cellColor = COLOR_CURRENT;
+        } else if (cell.isVisited) {
+            cellColor = COLOR_VISITED;
+        }
+
+        g2d.setColor(cellColor);
+        g2d.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+
+        // Draw cell border
+        g2d.setColor(Color.LIGHT_GRAY);
+        g2d.drawRect(x, y, CELL_SIZE, CELL_SIZE);
+
+        // Draw start and end points on top of everything
+        if (cell.isStart) {
+            g2d.setColor(COLOR_START);
+            g2d.fillOval(x + CELL_SIZE / 4, y + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2);
+        } else if (cell.isEnd) {
+            g2d.setColor(COLOR_END);
+            g2d.fillRect(x + CELL_SIZE / 4, y + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2);
+        }
+
+        // Display cell weight (only if different from 1 and not a special cell)
+        if (cell.weight > 1 && !cell.isWall && !cell.isStart && !cell.isEnd) {
+            g2d.setColor(Color.DARK_GRAY);
+            g2d.setFont(new Font("Arial", Font.BOLD, CELL_SIZE / 3));
+            String weightStr = String.valueOf(cell.weight);
+            FontMetrics fm = g2d.getFontMetrics();
+            int strX = x + (CELL_SIZE - fm.stringWidth(weightStr)) / 2;
+            int strY = y + (fm.getAscent() + (CELL_SIZE - fm.getHeight())) / 2;
+            g2d.drawString(weightStr, strX, strY);
+        }
+    }
 }
